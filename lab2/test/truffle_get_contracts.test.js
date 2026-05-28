cdab06f19afc55f95ec92dc20a469448da6c6ab2
Joyce Cai <cmt801107@gmail.com>
2026-05-28 13:52:18 +0800

    new contract practice

--- lab2/test/truffle_get_contracts.test.js
+++ lab2/test/truffle_get_contracts.test.js
@@ -2,9 +2,12 @@ const Chat = artifacts.require("../contracts/Chat")
 const assert = require("assert")
 contract("測試Call", () => {
     let chat
+    const defaultVaule = "A234567890";
     beforeEach(async () => {
         // 初始化合約object
-        chat = await Chat.deployed()
+        //chat = await Chat.deployed()
+        chat = await Chat.new(defaultVaule)
+
         console.log("chat 合約 address 是:", chat.address)
     })
     it("合約部署了", async () => {
@@ -13,7 +16,7 @@ contract("測試Call", () => {
     })
     it("合約互動, 取得預設值", async () => {
         const r1 = await chat.message();
-        assert.strictEqual(await chat.message(), "this is a formal document regarding...");
+        assert.strictEqual(await chat.message(), defaultVaule);
     })
     it("合約互動, 設值並且取得新值", async () => {
         const newMessage = "change to something";
@@ -21,9 +24,9 @@ contract("測試Call", () => {
         const newReturn = await chat.getMessage()
         assert.strictEqual(newReturn, newMessage)
     })
-    // it("合約互動, 重新取得預設計值", async () => {
-    //     const r1 = await chat.message();
-    //     assert.strictEqual(await chat.message(), "this is a formal document regarding...");
+    it("合約互動, 重新取得預設計值", async () => {
+        const r1 = await chat.message();
+        assert.strictEqual(await chat.message(), defaultVaule);
 
-    // })
+    })
 })
